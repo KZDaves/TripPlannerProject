4d0fb49fcb5d253b2592234695d0e100f53996a3
KZD <45324536+KZDaves@users.noreply.github.com>
2019-02-08 22:48:28 -0800

    rough landing page, created html file for exisiting plans, initialized FireBase, removed trial html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyARhBOFgmMf_9cj5S_Eq3zKI6D4AqCNIz4",
+    authDomain: "tripplanner-47fd1.firebaseapp.com",
+    databaseURL: "https://tripplanner-47fd1.firebaseio.com",
+    projectId: "tripplanner-47fd1",
+    storageBucket: "tripplanner-47fd1.appspot.com",
+    messagingSenderId: "556604987785"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+
